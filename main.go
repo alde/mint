80c3c02bf3af2ef2d2b2fa610058726eee0f9329
Rickard Dybeck <dybeck@spotify.com>
2024-02-27 21:38:46 -0500

    chapter 1.5 done

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"alde.nu/mint/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language REPL!\n", user.Username)
+
+	fmt.Printf("Try out my language by typing in commands\n")
+
+	repl.Start(os.Stdin, os.Stdout)
+
+}
