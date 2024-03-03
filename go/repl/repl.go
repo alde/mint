e219d85782f891c87630e2b62c801782f2d98b31
Rickard Dybeck <dybeck@spotify.com>
2024-03-03 14:41:15 -0500

    turn RLPL -> RPPL | chapter 2.9 done
    Read Lex Print Loop -> Read Parse Print Loop

--- go/repl/repl.go
+++ go/repl/repl.go
@@ -6,25 +6,49 @@ import (
 	"io"
 
 	"alde.nu/mint/lexer"
-	"alde.nu/mint/token"
+	"alde.nu/mint/parser"
+	"github.com/fatih/color"
 )
 
 const PROMPT = ">> "
 
-func Start(in io.Reader, out io.Writer) {
-	scanner := bufio.NewScanner(in)
+var (
+	yellow = color.New(color.FgYellow).SprintFunc()
+	red    = color.New(color.FgRed).SprintFunc()
+	green  = color.New(color.FgGreen).SprintFunc()
+)
 
+func Start(in io.Reader, out io.Writer) {
+
+	scanner := bufio.NewScanner(in)
+	num := 0
 	for {
 		fmt.Fprint(out, PROMPT)
 		scanned := scanner.Scan()
 		if !scanned {
 			return
 		}
-
+		num += 1
 		line := scanner.Text()
 		l := lexer.Create(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		p := parser.Create(l)
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+		index := fmt.Sprintf("%s%s%s ", green("["), yellow(num), green("]"))
+		io.WriteString(out, index)
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, red("Parser Errors:\n"))
+	for _, msg := range errors {
+		err := fmt.Sprintf("\t%s\n", red(msg))
+		io.WriteString(out, err)
 	}
 }
