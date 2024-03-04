a434cc01cda5493d5befff2060df546921d47f2c
Rickard Dybeck <dybeck@spotify.com>
2024-03-04 16:54:11 -0500

    chapter 3.6 - if/else

--- go/evalutator/evaluator.go
+++ go/evalutator/evaluator.go
@@ -31,6 +31,10 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return nativeBooleanToBooleanObject(node.Value)
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
+	case *ast.IfExpression:
+		return evalIfExpression(node)
 	}
 
 	return nil
@@ -132,4 +136,28 @@ func nativeBooleanToBooleanObject(val bool) object.Object {
 		return TRUE
 	}
 	return FALSE
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	} else {
+		return NULL
+	}
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case TRUE:
+		return true
+	case FALSE:
+		return false
+	default:
+		return true
+	}
 }
--- go/evalutator/evaluator_test.go
+++ go/evalutator/evaluator_test.go
@@ -89,6 +89,32 @@ func Test_BangOperator(t *testing.T) {
 	}
 }
 
+func Test_IfElseExpression(t *testing.T) {
+	testData := []struct {
+		input    string
+		expected interface{}
+	}{
+		{"if (true) { 10 }", 10},
+		{"if (false) { 10 }", nil},
+		{"if (1) { 10 }", 10},
+		{"if (1<2) { 10 }", 10},
+		{"if (1>2) { 10 }", nil},
+		{"if (1>2) { 10 } else { 20 }", 20},
+		{"if (1<2) { 10 } else { 20 }", 10},
+	}
+
+	for _, tt := range testData {
+		evaluated := testEval(tt.input)
+
+		integer, ok := tt.expected.(int)
+		if ok {
+			testIntegerObject(t, evaluated, int64(integer))
+		} else {
+			testNullObject(t, evaluated)
+		}
+	}
+}
+
 /// Helper functions /////////////////////////////////////////////////
 
 func testEval(input string) object.Object {
@@ -118,6 +144,14 @@ func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
 	}
 	if result.Value != expected {
 		t.Errorf("object has wrong value, got %t, want %t", result.Value, expected)
+		return false
+	}
+	return true
+}
+
+func testNullObject(t *testing.T, obj object.Object) bool {
+	if obj != NULL {
+		t.Errorf("expected object to be NULL, got %T (%+v)", obj, obj)
 		return false
 	}
 	return true
