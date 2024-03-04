8d53677056019de8b9a4025e9472e51af3ac4fc5
Rickard Dybeck <dybeck@spotify.com>
2024-03-04 09:41:03 -0500

    Chapter 3.4 - starting the evaluator

---
+++ go/object/object.go
@@ -0,0 +1,13 @@
+package object
+
+type ObjectType string
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+const (
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
+)
